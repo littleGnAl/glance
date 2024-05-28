2b1b17e648341c40a1a977b96c4eeee545cd57a7
fenglang <fenglang@MAGIT02814.local>
2024-05-28 19:09:33 +0800

    ++

---
+++ bin/symbolize.dart
@@ -0,0 +1,11 @@
+import 'package:file/file.dart' as file;
+import 'package:file/local.dart';
+import 'package:path/path.dart' as path;
+
+void main(List<String> arguments) {
+const file.FileSystem fileSystem = LocalFileSystem();
+}
+
+void _symbolize(file.FileSystem fileSystem, String symbolFilePath) {
+  
+}
