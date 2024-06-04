4e8a7d29644051028e74d6f7d1559dbb38dab946
fenglang <fenglang@MAGIT02814.local>
2024-06-04 16:37:34 +0800

    ++

--- lib/src/glance_internal.dart
+++ lib/src/glance_internal.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:isolate';
+import 'dart:nativewrappers/_internal/vm/lib/ffi_allocation_patch.dart';
 import 'dart:ui';
 
 import 'package:flutter/scheduler.dart';
@@ -9,49 +10,89 @@ import 'package:glance/src/collect_stack.dart';
 /// 16ms
 const int _kDefaultJankThreshold = 16;
 
+typedef JankCallback = void Function(StackTrace stacktrace);
+
+class StackTrace {
+  const StackTrace._(this.frames);
+  final List<NativeFrame> frames;
+}
+
 class GlanceConfiguration {
-  const GlanceConfiguration({this.jankThreshold = _kDefaultJankThreshold});
-  final int jankThreshold;
-}
-
-Future<void> glance(GlanceConfiguration? config) async {
-  final SampleThread sampleThread = await SampleThread.spawn();
-
-  final jankThreshold = config?.jankThreshold ?? _kDefaultJankThreshold;
-  SchedulerBinding.instance.addTimingsCallback((List<FrameTiming> timings) {
-    int now = DateTime.now().millisecondsSinceEpoch;
-    for (int i = 0; i < timings.length; ++i) {
-      final timing = timings[i];
-      final totalSpan = timing.totalSpan.inMilliseconds;
-      if (now - totalSpan > jankThreshold) {
-        // report jank
-        _report(timings, i);
-        break;
-      }
-    }
+  const GlanceConfiguration({
+    this.jankThreshold = _kDefaultJankThreshold,
+    this.jankCallback,
   });
+  final int jankThreshold;
+  final JankCallback? jankCallback;
 }
 
-Future<void> _report(List<FrameTiming> timings, int index) async {
-  // Report the nearest 3 timings if possiable.
-  int preIndex = index - 1;
-  int nextIndex = index + 1;
-  List<FrameTiming> reportTimings = [];
-  if (preIndex >= 0) {
-    reportTimings.add(timings[preIndex]);
-  }
-  reportTimings.add(timings[index]);
-  if (nextIndex < timings.length) {
-    reportTimings.add(timings[nextIndex]);
-  }
-  assert(reportTimings.isNotEmpty);
+class Glance {
+  Glance._();
 
-  // Request stacktraces
-  final timestampRange = [
-    reportTimings.first.timestampInMicroseconds(FramePhase.buildStart),
-    reportTimings.last.timestampInMicroseconds(FramePhase.rasterFinish),
-  ];
+  static Glance? _instance;
+  static Glance get instance {
+    _instance ??= Glance._();
+    return _instance!;
+  }
+
+  SampleThread? _sampleThread;
+
+  final List<JankCallback> _jankCallbacks = [];
+
+  Future<void> start(GlanceConfiguration? config) async {
+    _sampleThread ??= await SampleThread.spawn();
+
+    final jankThreshold = config?.jankThreshold ?? _kDefaultJankThreshold;
+    SchedulerBinding.instance.addTimingsCallback((List<FrameTiming> timings) {
+      int now = DateTime.now().millisecondsSinceEpoch;
+      for (int i = 0; i < timings.length; ++i) {
+        final timing = timings[i];
+        final totalSpan = timing.totalSpan.inMilliseconds;
+        if (now - totalSpan > jankThreshold) {
+          // report jank
+          _report(timings, i);
+          break;
+        }
+      }
+    });
+  }
+
+  Future<void> _report(List<FrameTiming> timings, int index) async {
+    // Report the nearest 3 timings if possiable.
+    int preIndex = index - 1;
+    int nextIndex = index + 1;
+    List<FrameTiming> reportTimings = [];
+    if (preIndex >= 0) {
+      reportTimings.add(timings[preIndex]);
+    }
+    reportTimings.add(timings[index]);
+    if (nextIndex < timings.length) {
+      reportTimings.add(timings[nextIndex]);
+    }
+    assert(reportTimings.isNotEmpty);
+
+    // Request stacktraces
+    final timestampRange = [
+      reportTimings.first.timestampInMicroseconds(FramePhase.buildStart),
+      reportTimings.last.timestampInMicroseconds(FramePhase.rasterFinish),
+    ];
+
+    assert(_sampleThread != null);
+    final frames = await _sampleThread!.getSamples(timestampRange);
+    final stacktrace = StackTrace._(frames);
+
+    final callbacks = List.from(_jankCallbacks);
+    for (final callback in callbacks) {
+      callback(stacktrace);
+    }
+  }
+
+  void addJankCallback(JankCallback callback) {
+    _jankCallbacks.add(callback);
+  }
 }
+
+
 
 // import 'dart:async';
 // import 'dart:convert';
