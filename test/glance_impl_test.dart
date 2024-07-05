08d461e6a81118eaabb7024e374809e4cdae48db
fenglang <fenglang@MAGIT02814.local>
2024-07-05 14:02:56 +0800

    ++

--- test/glance_impl_test.dart
+++ test/glance_impl_test.dart
@@ -5,26 +5,19 @@ import 'dart:ui';
 import 'package:fake_async/fake_async.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:glance/glance.dart';
-import 'package:glance/glance_platform_interface.dart';
-import 'package:glance/glance_method_channel.dart';
 import 'package:glance/src/collect_stack.dart';
 import 'package:glance/src/glance_impl.dart';
 import 'package:glance/src/sampler.dart';
-import 'package:plugin_platform_interface/plugin_platform_interface.dart';
-
-// class MockGlancePlatform
-//     with MockPlatformInterfaceMixin
-//     implements GlancePlatform {
-
-//   @override
-//   Future<String?> getPlatformVersion() => Future.value('42');
-// }
 
 class FakeSampler implements Sampler {
   List<AggregatedNativeFrame> frames = [];
 
+  bool isClose = false;
+
   @override
-  void close() {}
+  void close() {
+    isClose = true;
+  }
 
   @override
   Future<List<AggregatedNativeFrame>> getSamples(
@@ -53,17 +46,14 @@ void main() {
     glance = GlanceImpl.forTesting(sampler);
   });
 
-  test('should receive a report callback if stace traces are not empty',
+  test('Should receive a report callback if stace traces are not empty',
       () async {
     final reportCompleter = Completer<JankReport>();
-    // late JankReport report;
     await glance.start(
       config: GlanceConfiguration(
         jankThreshold: 1,
         reporters: [
           TestJankDetectedReporter((info) {
-            // report = info;
-
             if (!reportCompleter.isCompleted) {
               reportCompleter.complete(info);
             }
@@ -108,7 +98,7 @@ void main() {
     expect(report, equals(expectedReport));
   });
 
-  test('should not receive a report callback if stace traces are empty',
+  test('Should not receive a report callback if stace traces are empty',
       () async {
     fakeAsync((async) {
       final reportCompleter = Completer<JankReport>();
@@ -147,7 +137,7 @@ void main() {
     });
   });
 
-  test('should not receive a report callback if stace traces are the same',
+  test('Should not receive a report callback if stace traces are the same',
       () async {
     fakeAsync((async) async {
       final reportCompleter = Completer<JankReport>();
@@ -215,13 +205,64 @@ void main() {
     });
   });
 
-  test('should not receive a report callback after calling end', () {});
+  test('Call Sampler.close after calling end', () async {
+    glance.start();
+    await glance.end();
 
-  // test('getPlatformVersion', () async {
-  //   Glance glancePlugin = Glance();
-  //   MockGlancePlatform fakePlatform = MockGlancePlatform();
-  //   GlancePlatform.instance = fakePlatform;
+    expect(sampler.isClose, isTrue);
+  });
 
-  //   expect(await glancePlugin.getPlatformVersion(), '42');
-  // });
+  test('Should not receive a report callback after calling end', () {
+    fakeAsync((async) async {
+      // Add a TimingsCallback to initialize the `PlatformDispatcher.onReportTimings`
+      binding.addTimingsCallback((timings) {});
+      final onReportTimings = binding.platformDispatcher.onReportTimings;
+
+      final reportCompleter = Completer<JankReport>();
+      glance.start(
+        config: GlanceConfiguration(
+          jankThreshold: 1,
+          reporters: [
+            TestJankDetectedReporter((info) {
+              if (!reportCompleter.isCompleted) {
+                reportCompleter.complete(info);
+              }
+            }),
+          ],
+        ),
+      );
+
+      await glance.end();
+
+      final frame = AggregatedNativeFrame(NativeFrame(
+        pc: 540642472608,
+        timestamp: Timeline.now,
+        module: NativeModule(
+          id: 1,
+          path: 'libapp.so',
+          baseAddress: 540641718272,
+          symbolName: 'hello',
+        ),
+      ));
+      final frames = [frame];
+      sampler.frames = frames;
+
+      final rasterFinish = Timeline.now - 1000;
+      final timing = FrameTiming(
+        vsyncStart: rasterFinish - 4000,
+        buildStart: rasterFinish - 3000,
+        buildFinish: rasterFinish - 2000,
+        rasterStart: rasterFinish - 1000,
+        rasterFinish: rasterFinish,
+        rasterFinishWallTime: rasterFinish,
+      );
+      final timings = [timing];
+      onReportTimings!(timings);
+
+      expect(reportCompleter.future.timeout(const Duration(seconds: 5)),
+          throwsA(isA<TimeoutException>()));
+
+      async.elapse(const Duration(seconds: 5));
+    });
+  });
 }
