a30f4e86ecd3feeacb8d229b2662be7865d1361a
littleGnAl <tearjeaker@hotmail.com>
2024-07-11 01:43:16 +0800

    ++

--- test/collect_stack_test.dart
+++ test/collect_stack_test.dart
@@ -1,3 +1,4 @@
+import 'dart:developer';
 import 'dart:ffi' as ffi;
 import 'package:ffi/ffi.dart';
 
@@ -20,10 +21,12 @@ class FakeCollectStackNativeBindings implements CollectStackNativeBindings {
   ffi.Pointer<Utf8> CollectStackTraceOfTargetThread(
       ffi.Pointer<ffi.Int64> buf, int bufSize) {
     isCollectStackTraceOfTargetThread = true;
+    buf[0] = 123;
     return ffi.nullptr; // success
   }
 
   @override
+  // ignore: non_constant_identifier_names
   int Dladdr(ffi.Pointer<ffi.Void> addr, ffi.Pointer<DlInfo> info) {
     isDladdr = true;
     info.ref.fileName = "libapp.so".toNativeUtf8(allocator: arena);
@@ -31,10 +34,11 @@ class FakeCollectStackNativeBindings implements CollectStackNativeBindings {
     info.ref.symbolName = "hello".toNativeUtf8(allocator: arena);
     info.ref.symbolAddress = ffi.Pointer<ffi.Void>.fromAddress(456);
 
-    return 0; // found
+    return 1; // found
   }
 
   @override
+  // ignore: non_constant_identifier_names
   ffi.Pointer<Utf8> LookupSymbolName(ffi.Pointer<DlInfo> info) {
     isLookupSymbolName = true;
     // Do not use the `arena` to allocate it, since it will be freed by the user
@@ -42,6 +46,7 @@ class FakeCollectStackNativeBindings implements CollectStackNativeBindings {
   }
 
   @override
+  // ignore: non_constant_identifier_names
   void SetCurrentThreadAsTarget() {
     isSetCurrentThreadAsTarget = true;
   }
@@ -62,6 +67,27 @@ void main() {
       });
     });
 
-    test('captureStackOfTargetThread', () {});
+    test('captureStackOfTargetThread', () {
+      using((arena) {
+        nativeBindings = FakeCollectStackNativeBindings(arena);
+        stackCapturer = StackCapturer(nativeBindings: nativeBindings);
+
+        final nativeStack = stackCapturer.captureStackOfTargetThread();
+        expect(nativeBindings.isCollectStackTraceOfTargetThread, isTrue);
+        expect(nativeBindings.isDladdr, isTrue);
+        expect(nativeBindings.isLookupSymbolName, isTrue);
+
+        final frame = nativeStack.frames[0];
+        final module = nativeStack.modules[0];
+        expect(frame.pc, 123);
+        expect(frame.module, module);
+        expect(frame.module, module);
+
+        expect(module.id, 0); // The first `id` is 0
+        expect(module.path, 'libapp.so');
+        expect(module.baseAddress, 123);
+        expect(module.symbolName, 'hello');
+      });
+    });
   });
 }
