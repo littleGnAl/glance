5e0f97b1ec0c73640ac69fb75b8e3a09b83b0b48
littleGnAl <tearjeaker@hotmail.com>
2024-07-21 18:16:44 +0800

    ++

--- scripts/glance_integration_test.sh
+++ scripts/glance_integration_test.sh
@@ -8,6 +8,12 @@ MY_PATH=$(dirname "$0")
 # PROJECT_ROOT=${MY_PATH}/..
 PLATFORM=$1 # android/ios/macos/windows/web
 
+# Export the llvm path
+# https://github.com/actions/runner-images/blob/main/images/macos/macos-12-Readme.md
+BREW_PREFIX=$(brew --prefix llvm@15)
+export PATH="${BREW_PREFIX}/bin:$PATH"
+which llvm-symbolizer
+
 pushd ${MY_PATH}/../example
 
 export SAVE_DEBUG_GOLDEN="true"
