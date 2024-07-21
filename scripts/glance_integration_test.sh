75b2a79fbcba86af013b50546be86c8ce657e642
littleGnAl <tearjeaker@hotmail.com>
2024-07-21 18:42:02 +0800

    ++

--- scripts/glance_integration_test.sh
+++ scripts/glance_integration_test.sh
@@ -8,13 +8,13 @@ MY_PATH=$(dirname "$0")
 # PROJECT_ROOT=${MY_PATH}/..
 PLATFORM=$1 # android/ios/macos/windows/web
 
-if [ $GITHUB_ACTIONS == "true"]; then
-    # Export the llvm path if running on CI
-    # https://github.com/actions/runner-images/blob/main/images/macos/macos-12-Readme.md
-    BREW_PREFIX=$(brew --prefix llvm@15)
-    export PATH="${BREW_PREFIX}/bin:$PATH"
-    which llvm-symbolizer
-fi
+# if [ $GITHUB_ACTIONS == "true" ]; then
+# Export the llvm path if running on CI
+# https://github.com/actions/runner-images/blob/main/images/macos/macos-12-Readme.md
+BREW_PREFIX=$(brew --prefix llvm@15)
+export PATH="${BREW_PREFIX}/bin:$PATH"
+which llvm-symbolizer
+# fi
 
 
 pushd ${MY_PATH}/../example
