e1948486b1f519aafa1b9037d0ab3a4c116e085f
littleGnAl <tearjeaker@hotmail.com>
2024-10-12 17:19:15 +0800

    fix: support android 15 16k page size

--- scripts/check_android15_16k_page_alignment.sh
+++ scripts/check_android15_16k_page_alignment.sh
@@ -4,10 +4,6 @@
 
 SO_FILE="$1"
 
-RED="\e[31m"
-GREEN="\e[32m"
-ENDCOLOR="\e[0m"
-
 res="$(objdump -p ${SO_FILE} | grep LOAD | awk '{ print $NF }' | head -1)"
 if [[ $res =~ "2**14" ]] || [[ $res =~ "2**16" ]]; then
     echo -e "${SO_FILE}: ALIGNED ($res)"
