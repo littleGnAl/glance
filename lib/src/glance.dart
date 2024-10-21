7f32cbe5351ad073eeb0e1e0f60a9a5e2035c23b
littleGnAl <tearjeaker@hotmail.com>
2024-10-21 19:00:06 +0800

    ++

--- lib/src/glance.dart
+++ lib/src/glance.dart
@@ -88,6 +88,18 @@ class GlanceConfiguration {
 /// After obtaining the [JankReport.stackTrace], you can symbolize it using the `flutter symbolize` command.
 /// For more details, see https://docs.flutter.dev/deployment/obfuscate#read-an-obfuscated-stack-trace.
 /// You can save the stack traces to files or upload them to your server and symbolize them later.
+/// 
+/// ## How it works
+/// `glance` starts a dedicated [Isolate] internally to capture Dart UI thread stack traces using native stack unwinding. 
+/// Refer to [Sampler] for more details.
+/// 
+/// To detect UI jank, `glance` extends `WidgetsFlutterBinding`, monitoring the build phase execution time, 
+/// along with callbacks such as `WidgetBindingObserver`, touch events, and method channel callbacks. 
+/// Jank is detected when the execution time exceeds the [GlanceConfiguration.jankThreshold].
+/// For further information, refer to [GlanceWidgetBinding].
+/// 
+/// Upon detecting jank, `glance` fetches stack traces from the [Sampler] during the jank period and reconstructs them into Dart stack traces. 
+/// These are then reported to the [GlanceReporter], specified via [GlanceConfiguration.reporters].
 abstract class Glance {
   Glance._();
 
